dae0aaa2e068f3c0c67fdffe05e41ccf9a8b6e07
Eesa <eesak@outlook.com>
2020-09-01 21:53:34 +0100

    Changed Secret Key access

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '415*7uwk&3x=w_t@5!v&ok!3zi!e_fks&%u2!_*-bcij4xw!%='
+# SECRET_KEY = '415*7uwk&3x=w_t@5!v&ok!3zi!e_fks&%u2!_*-bcij4xw!%='
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
